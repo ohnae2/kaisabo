abe694ee9c10977eb30743d25ace83fd6cc325a2
kaisa <kaisaohnae@gmail.com>
2023-03-20 10:54:18 +0900

    init

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+npm run build
+cd dist
+Remove-Item -Recurse -Force .git
+echo 'bo.kaisa.co.kr' > CNAME
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+git remote add origin "https://github.com/kaisaohnae/kaisabo.git"
+git push -u --force origin main
+cd ..
