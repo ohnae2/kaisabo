90ed75b26732f4e91c072106c8958c0e6e09aee1
kaisa <kaisaohnae@gmail.com>
2023-03-28 08:39:26 +0900

    ..

--- src/views/bs/Dictionary.vue
+++ src/views/bs/Dictionary.vue
@@ -1,5 +1,26 @@
 <template>
 	<div id="dirctionary">
+		<form class="search" @submit.prevent="getList">
+			<fieldset>
+				<legend>검색</legend>
+				<table>
+					<colgroup>
+						<col width="100" />
+						<col width="*" />
+				</colgroup>
+					<tr>
+						<th>검색조건</th>
+						<td><input type="text" /></td>
+						<th>수정날짜</th>
+						<td><input type="text" /></td>
+					</tr>
+				</table>
+			</fieldset>
+			<div class="btnWrap">
+				<button type="submit" class="button3"><span class="icon">&#xe096;</span></button>
+				<button type="reset" class="refresh"><span class="icon">&#x22;</span></button>
+			</div>
+		</form>
 		<div id="grid"></div>
 	</div>
 </template>
@@ -11,20 +32,51 @@ import DictionaryService from '../../service/bs/DictionaryService';
 const data = reactive({
 grid: {} as any, 
 });
+
+const getList = function () {
+	DictionaryService.getDictionaryList().then(
+		(res) => {
+			let idx = 0;
+			for(let o of res.data) {
+				o.rowIdx = idx++;
+			}
+			data.grid?.resetData(res.data, {});
+		},
+		(err) => {
+			console.log(err);
+		},
+	);
+}
+
 onMounted(() => {
 	data.grid = new Grid({
 		el: document.getElementById('grid') as HTMLElement,
 		columns: [
-			{header: 'abb', name: 'abb', editor: 'text'},
-			{header: 'ko', name: 'ko', editor: 'text'},
+			{header: 'abb', name: 'abb', editor: 'text', sortable: true, align: 'center' },
+			{header: 'ko', name: 'ko', editor: 'text', hidden: false},
+			{header: 'ko', name: 'ko', editor: 'text', hidden: false, width: 80, 
+				/*formatter: 'listItemText',
+				editor: {
+					type: 'select',
+					options: {
+					listItems: [
+						{ text: '', value: '', dataType: 'string'},
+						{ text: '접수', value: '20', dataType: 'string'},
+						{ text: '관찰', value: '30', dataType: 'string'},
+						{ text: '완료', value: '40', dataType: 'string'},
+					]
+					},
+				},*/
+			},
 			{header: 'en', name: 'en', editor: 'text'},
 			{header: 'dsc', name: 'dsc', editor: 'text'},
-			{header: 'linkRef', name: 'linkRef', editor: 'text'},
+			{header: 'linkRef', name: 'linkRef', editor: 'text', defaultValue: ''},
+			{header: 'modDat', name: 'modDt', editor: 'text', disabled: false},
 		],
 		scrollX: true,
 		scrollY: true,
 		minBodyHeight: 200,
-		bodyHeight: 500,
+		bodyHeight: 468,
 		columnOptions: {
 			resizable: true,
 		},
@@ -40,22 +92,6 @@ onMounted(() => {
 			console.log('click')
 		}
 	});
-
-	const getList = function () {
-		DictionaryService.getDictionaryList().then(
-			(res) => {
-				let idx = 0;
-				for(let o of res.data) {
-					o.rowIdx = idx++;
-				}
-				data.grid?.resetData(res.data, {});
-			},
-			(err) => {
-				console.log(err);
-			},
-		);
-	}
-
 	getList();
 });
 
